e3bd130a102441b1c07bdefa260b92a02c47a3d1
Jacob Salzberg <jsalzbergedu@yahoo.com>
2017-03-05 15:39:35 -0500

    Still no changes functionally. Updating to backup data.

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ pByStudent = Path('By-Student')
 pByCamp = Path('By-Camp')
 
 if not pByStudent.exists() and not pByCamp.exists():
-	pByStudent.mkdir()
-	pByCamp.mkdir()
+    pByStudent.mkdir()
+    pByCamp.mkdir()
 
-## some input file is parsed
-## TODO automate file to open
+
+# Some input file is parsed
+# TODO automate file to open
 changeString = '-unused-'
 studentsNameString = '-unused-'
 ageString = '-unused-'
@@ -17,7 +18,33 @@ upcomingGradeString = '-unused-'
 streetAdressString = '-unused-'
 cityString = '-unused-'
 zipString = '-unused-'
-lastGradeSchool = '-unused-'
+lastGradeSchoolString = '-unused-'
+parentsNameString = '-unused-'
+parentsPhoneString = '-unused-'
+parentsEmailString = '-unused-'
+programAndDateString = '-unused-'
+costString = '-unused-'
+programAndDateStringII = '-unused-'
+costStringII = '-unused-'
+programAndDateStringIII = '-unused-'
+costStringIII = '-unused-'
+eParentsHomePhoneString = '-unused-'
+eMothersWorkPhoneString = '-unused-'
+eMothersCellPhoneString = '-unused-'
+eFathersWorkPhoneString = '-unused-'
+eFathersCellPhoneString = '-unused-'
+eContactNameString = '-unused-'
+eContactPhoneString = '-unused-'
+eContactCellPhoneString = '-unused-'
+localHospitalPreferenceString = '-unused'
+medAllergiesString = '-unused-'
+medAsthmaString = '-unused-'
+medEpilepsyString = '-unused-'
+medHeartProblemsString = '-unused-'
+medDrugAllergiesString = '-unused-'
+medRecurringIlnessString = '-unused-'
+medOtherString = '-unused-'
+medExplainString = '-unused-'
 dossier = open('igmeTest1.txt')
 '''
 for rangee in dossier
